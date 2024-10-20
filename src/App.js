3b8d600b1b8efbb3e64a3b459f3e55a91660feab
ZichengHe-Susan <bgc4bx@virginia.edu>
2024-10-20 15:07:59 -0400

    fixed conflicts

--- src/App.js
+++ src/App.js
@@ -8,11 +8,11 @@ import { CartProvider } from './contexts/CartContext';
 import AddItem from './pages/Upload';
 import ViewItems from './pages/ViewItems';
 import ShoppingCart from './pages/ShoppingCart';
-<<<<<<< HEAD
+
 import ItemDetails from './pages/ItemDetail';
-=======
+
 import Checkout from './pages/Checkout';
->>>>>>> main
+
 
 function App() {
   return (
@@ -25,11 +25,8 @@ function App() {
           <Route path="/upload" element={<AddItem />} />
           <Route path="/items" element={<ViewItems />} />
           <Route path="/cart" element={<ShoppingCart />} />
-<<<<<<< HEAD
           <Route path="/item/:id" element={<ItemDetails />} />
-=======
           <Route path="/checkedOut" element={<Checkout />} />
->>>>>>> main
         </Routes>
         </CartProvider>
       </AuthProvider>
