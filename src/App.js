ba5d3d22dd49b7c2a13a860f8ea6077fca2e2d5d
Ninglan Lei <dwq3ve@virginia.edu>
2024-10-20 13:44:11 -0400

    - added DetailedItem page but not showing properly (blinks)

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import DetailedItem from "./pages/DetailedItem";
 
-import Login from './pages/Login';
-import { AuthProvider } from './contexts/AuthContext'; 
-import { CartProvider } from './contexts/CartContext';
-import AddItem from './pages/Upload';
-import ViewItems from './pages/ViewItems';
-import ShoppingCart from './pages/ShoppingCart';
+import Login from "./pages/Login";
+import { AuthProvider } from "./contexts/AuthContext";
+import { CartProvider } from "./contexts/CartContext";
+import AddItem from "./pages/Upload";
+import ViewItems from "./pages/ViewItems";
+import ShoppingCart from "./pages/ShoppingCart";
 
 function App() {
   return (
     <Router>
       <AuthProvider>
         <CartProvider>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/upload" element={<AddItem />} />
-          <Route path="/items" element={<ViewItems />} />
-          <Route path="/cart" element={<ShoppingCart />} />
-        </Routes>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/upload" element={<AddItem />} />
+            <Route path="/items" element={<ViewItems />} />
+            <Route path="/cart" element={<ShoppingCart />} />
+            <Route path="/item/:itemId" element={<DetailedItem />} />
+          </Routes>
         </CartProvider>
       </AuthProvider>
     </Router>
